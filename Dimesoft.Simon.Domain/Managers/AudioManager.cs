e756d242e8da943caf1849f7602a1c5e2551a863
Derik Whittaker <derik@graudo.com>
2012-08-18 12:56:33 -0400

    Initial add of managers

---
+++ Dimesoft.Simon.Domain/Managers/AudioManager.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.UI.Xaml.Controls;
+
+namespace Dimesoft.Simon.Domain.Managers
+{
+    public interface IAudioManager
+    {
+        Task Play(string fileName);
+        //Task PlayItemSelected();
+        //Task PlaySelectedItemMatch();
+        //Task PlayWonGame();
+    }
+
+    public class AudioManager : IAudioManager
+    {
+        private MediaElement _mediaElement;
+
+        public async Task Play(string fileName)
+        {
+            var packageLocation = Windows.ApplicationModel.Package.Current.InstalledLocation;
+            var assetsFolder = await packageLocation.GetFolderAsync("assets");
+            var soundsFolder = await assetsFolder.GetFolderAsync("sounds");
+            StorageFile myAudio = await soundsFolder.GetFileAsync(fileName);
+
+            _mediaElement = new MediaElement();
+
+            var stream = await myAudio.OpenAsync(FileAccessMode.Read);
+            _mediaElement.SetSource(stream, myAudio.ContentType);
+
+            _mediaElement.Play();
+        }
+
+        //public async Task PlayItemSelected()
+        //{
+        //    if (!_storageManager.Preferences().Result.PlayAudio) { return; }
+
+        //    await Play("SelectItemAudio.mp3");
+        //}
+
+        //public async Task PlaySelectedItemMatch()
+        //{
+        //    if (!_storageManager.Preferences().Result.PlayAudio) { return; }
+
+        //    await Play("SelectedItemMatchAudio.mp3");
+        //}
+
+        //public async Task PlayWonGame()
+        //{
+        //    if (!_storageManager.Preferences().Result.PlayAudio) { return; }
+
+        //    await Play("WonGameAudio.mp3");
+        //}
+    }
+}
---
+++ Dimesoft.Simon.Domain/Managers/PinManager.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.UI.StartScreen;
+
+namespace Dimesoft.Simon.Domain.Managers
+{
+    public interface IPinManager
+    {
+
+        bool IsPinned(string pinnedItemId);
+
+        Task<bool> Pin(string shortName, string description, string tileId, string tileActivationArgs, string tileLogoPath, string smallTileLogoPath);
+        Task<bool> UnPin(string pinnedItemId);
+
+        //void UpdateMainLiveTile();
+        //void UpdateSecondaryTiles();
+    }
+
+    public class PinManager : IPinManager
+    {
+
+        public bool IsPinned(string pinnedItemId)
+        {
+            var result = SecondaryTile.Exists(pinnedItemId);
+
+            return result;
+        }
+
+        public async Task<bool> Pin(string shortName, string description, string tileId, string tileActivationArgs, string tileLogoPath, string smallTileLogoPath)
+        {
+            var logo = new Uri(string.Format("ms-appx:///{0}", tileLogoPath));
+            var smallLogo = new Uri(string.Format("ms-appx:///{0}", smallTileLogoPath));
+
+            var tile = new SecondaryTile(tileId, shortName, description, tileActivationArgs, TileOptions.ShowNameOnLogo, logo)
+                           {
+                               ForegroundText = ForegroundText.Light, 
+                               SmallLogo = smallLogo
+                           };
+
+            var result = await tile.RequestCreateAsync();
+
+            return result;
+        }
+
+        public async Task<bool> UnPin(string pinnedItemID)
+        {
+            if (IsPinned(pinnedItemID))
+            {
+                var tile = new SecondaryTile(pinnedItemID);
+                var result = await tile.RequestDeleteAsync();
+
+                return result;
+            }
+
+            return false;
+        }
+
+        //public async void UpdateMainLiveTile()
+        //{
+        //    var applicationTile = TileContentFactory.CreateTileWidePeekImage02(); //.CreateTileWidePeekImageAndText01();
+        //    var squareApplicationTile = TileContentFactory.CreateTileSquarePeekImageAndText01();
+        //    var tileUpdater = TileUpdateManager.CreateTileUpdaterForApplication(CoreApplication.Id);
+
+        //    // clear the existing tile info
+        //    tileUpdater.Clear();
+
+        //    var results = await _storageManager.TopLeaderBoardResultsAsync();
+
+        //    if (!results.Any()) { return; }
+
+        //    var asArray = results.ToArray();
+        //    var item1 = TryGetAppTileArrayItem(asArray, 0);
+        //    var item2 = TryGetAppTileArrayItem(asArray, 1);
+        //    var item3 = TryGetAppTileArrayItem(asArray, 2);
+        //    var item4 = TryGetAppTileArrayItem(asArray, 3);
+
+        //    applicationTile.TextHeading.Text = "Top Scores";
+        //    applicationTile.TextBody1.Text = item1;
+        //    applicationTile.TextBody2.Text = item2;
+        //    applicationTile.TextBody3.Text = item3;
+        //    applicationTile.TextBody4.Text = item4;
+
+        //    applicationTile.RequireSquareContent = false;
+        //    applicationTile.Image.Src = "/Assets/WidePinTile.png";
+        //    applicationTile.SquareContent = squareApplicationTile;
+
+        //    var tileNotification = applicationTile.CreateNotification();
+        //    tileUpdater.Update(tileNotification);
+        //}
+
+        //public async void UpdateSecondaryTiles()
+        //{
+        //    var allTiles = await SecondaryTile.FindAllAsync();
+
+        //    foreach (var secondaryTile in allTiles)
+        //    {
+        //        var parts = secondaryTile.TileId.Split('.');
+        //        if (parts.Any())
+        //        {
+        //            var board = parts[1];
+        //            var level = parts[2];
+
+        //            var foundItems = await _storageManager.LeaderBoardResultsAsync(board, level);
+
+        //            if (foundItems.Any())
+        //            {
+
+        //                var tileImageSrc = _optionsFactory.SecondaryTileForCategory(foundItems.First().GameCategory);
+
+        //                var orderedResults = foundItems.OrderBy(x => x.Attempts).ToArray();
+        //                var item1 = TryGetSecondaryTileArrayItem(orderedResults, 0);
+        //                var item2 = TryGetSecondaryTileArrayItem(orderedResults, 1);
+        //                var item3 = TryGetSecondaryTileArrayItem(orderedResults, 2);
+
+        //                var updatableSecondaryTile = TileContentFactory.CreateTileSquarePeekImageAndText01();
+        //                var tileUpdater = TileUpdateManager.CreateTileUpdaterForSecondaryTile(secondaryTile.TileId);
+        //                tileUpdater.Clear();
+
+        //                updatableSecondaryTile.Image.Src = tileImageSrc;
+        //                updatableSecondaryTile.TextHeading.Text = "Scores";
+        //                updatableSecondaryTile.TextBody1.Text = item1;
+        //                updatableSecondaryTile.TextBody2.Text = item2;
+        //                updatableSecondaryTile.TextBody3.Text = item3;
+
+        //                var tileNotification = updatableSecondaryTile.CreateNotification();
+        //                tileUpdater.Update(tileNotification);
+
+        //            }
+        //        }
+        //    }
+        //}
+
+        //private static string TryGetAppTileArrayItem(GameResultDTO[] asArray, int index)
+        //{
+        //    try
+        //    {
+        //        var asDto = asArray[index];
+
+        //        return string.Format("{0} [{1}] in {2} attempts by {3}", asDto.GameCategory, asDto.GameLevel, asDto.Attempts, asDto.PlayerName);
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        return "";
+        //    }
+        //}
+
+        //private static string TryGetSecondaryTileArrayItem(GameResultDTO[] asArray, int index)
+        //{
+        //    try
+        //    {
+        //        var asDto = asArray[index];
+
+        //        return string.Format("{0} Moves: {1}", asDto.Attempts, asDto.PlayerName);
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        return "";
+        //    }
+        //}
+
+    }
+}
